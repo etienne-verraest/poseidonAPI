cc4ebe8577f32361a18c709e7d220045d9ed13aa
etienne-verraest <verraest.etienne@gmail.com>
2022-09-12 18:12:08 +0200

    Modified header on each page and created a header fragment to reuse

--- src/main/java/com/poseidon/app/controllers/HomeController.java
+++ src/main/java/com/poseidon/app/controllers/HomeController.java
@@ -7,6 +7,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
+	/**
+	 * We ensure that the main page of the application is the bid list page.
+	 * The Default Success Url on Spring security is also set on this url.
+	 */
 	@RequestMapping("/")
 	public String home(Model model) {
 		return "redirect:/bidList/list";
